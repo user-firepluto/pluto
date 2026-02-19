279177fb83f4a2f591f2144b5b3b7642ccd45c64
user-firepluto <eduardovianapires987@gmail.com>
2026-02-19 15:14:13 -0300

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+chmod +x pluto-i
+chmod +x pluto-r
+chmod +x pluto-h
+mkdir /home/$USER/.pluto-temp
+sudo cp pluto-i /usr/local/bin
+sudo cp pluto-r /usr/local/bin
+sudo cp pluto-h /usr/local/bin
---
+++ remove.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf /home/$USER/.pluto-temp
+sudo rm -rf /usr/local/bin/pluto-i
+sudo rm -rf /usr/local/bin/pluto-r
+sudo rm -rf /usr/local/bin/pluto-h
