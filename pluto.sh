88a7b4b51c6f40c8fe211a9e053ed9942526ffdf
user-firepluto <eduardovianapires987@gmail.com>
2026-02-19 15:13:51 -0300

    Delete pluto.sh

--- pluto.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-read -p "Program name: " name
-git clone "https://www.github.com/user-firepluto/$name"
-cd "$name" && chmod +x installer.sh && ./installer.sh
-rm -rf /home/$USER/"$name"
-echo "Installation of the program ($name) is sucefully completed"
