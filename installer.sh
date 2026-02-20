46c250927ebea7a805724d87bb4c4f28950ff5fe
user-firepluto <eduardovianapires987@gmail.com>
2026-02-20 14:49:19 -0300

    Delete installer.sh

--- installer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-chmod +x pluto-i
-chmod +x pluto-r
-chmod +x pluto-h
-mkdir /home/$USER/.pluto-temp
-sudo cp pluto-i /usr/local/bin
-sudo cp pluto-r /usr/local/bin
-sudo cp pluto-h /usr/local/bin
